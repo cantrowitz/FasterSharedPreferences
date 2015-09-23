2a160cfac56c00133d51e7a79b6fe34317349106
Adam Cantrowitz <acantrowitz@yahoo-inc.com>
2015-09-23 19:36:08 -0400

    added some testing

--- fastersharedpreferences-sample/src/main/java/com/cantrowitz/fastersharedpreferences/demo/MainActivity.java
+++ fastersharedpreferences-sample/src/main/java/com/cantrowitz/fastersharedpreferences/demo/MainActivity.java
@@ -1,15 +1,17 @@
 package com.cantrowitz.fastersharedpreferences.demo;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.cantrowitz.fastersharedpreferences.FasterSharedPreferences;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int NUMBER_OF_NAMES = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,40 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+        String[] names = generateNames();
 
+        long individualRunTime = runTestForIndividualFiles(names);
+        long fasterSPRunTime = runTestForFasterSP(names);
 
-
+        System.out.println("+++ Individual=" + individualRunTime + " ms FasterSP=" + fasterSPRunTime + "ms");
+        System.out.println("+++ chart \t" + individualRunTime + "\t" + fasterSPRunTime);
 
     }
 
+    private long runTestForFasterSP(String[] names) {
+        Context applicationContext = getApplicationContext();
+        long start = SystemClock.elapsedRealtime();
+        for (int i = 0; i < NUMBER_OF_NAMES; i++) {
+            FasterSharedPreferences.get(applicationContext, names[i], MODE_PRIVATE);
+        }
+        return SystemClock.elapsedRealtime() - start;
+    }
 
+    private long runTestForIndividualFiles(String[] names) {
+        Context applicationContext = getApplicationContext();
+        long start = SystemClock.elapsedRealtime();
+        for (int i = 0; i < NUMBER_OF_NAMES; i++) {
+            applicationContext.getSharedPreferences(names[i], MODE_PRIVATE);
+        }
+        return SystemClock.elapsedRealtime() - start;
+    }
+
+    @NonNull
+    private String[] generateNames() {
+        String[] names = new String[NUMBER_OF_NAMES];
+        for (int i = 0; i < NUMBER_OF_NAMES; i++) {
+            names[i] = String.format("prefs_%d", i);
+        }
+        return names;
+    }
 }
